4ee6e74c0938c90ed801654bd5bb657540957f94
Itai Zukerman <zukerman@math-hat.com>
2015-01-21 22:18:15 -0800

    Made the hash functions more generic via a type class.

--- src/Main.hs
+++ src/Main.hs
@@ -1,92 +1,104 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 import Control.Monad (foldM_, replicateM, (>=>))
 import Data.Array.IO (IOUArray)
 import qualified Data.Array.IO as A
 import Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString.Lazy as B
 import Data.Char (chr, ord)
-import Data.Word (Word64)
+import Data.Word (Word64, Word8)
 
 import Debug.Trace (trace)
 
-type Seed = Word64
+class Gen g a | g -> a where
+  zero :: g -> g
+  next :: a -> g -> g
+  index :: g -> (Int, Int) -> Int
 
-data LCG = LCG Word64 Word64 Word64
+reset :: Gen g a => [g] -> [g]
+reset = map zero
 
-rand :: Int -> LCG -> Seed -> Seed
-rand i (LCG m a c) s = (((s * a) `mod` m) + c + fromIntegral i) `mod` m
+data LCG = LCG
+           { lcgM :: Word64
+           , lcgA :: Word64
+           , lcgC :: Word64
+           , lcgS :: Word64
+           }
 
-type Counts = IOUArray Word64 Int
+instance Gen LCG Word8 where
+  zero g@(LCG _ _ c _) = g { lcgS = c }
+  next ch g@(LCG m a c s) = g { lcgS = (((s * a) `mod` m) + c + fromIntegral ch) `mod` m }
+  index g@(LCG _ _ _ s) (min, max) = (fromIntegral s `mod` (max - min + 1)) + min
 
+type Counts = IOUArray Int Int
 type Table = [Counts]
 
-empty :: [Seed]
-empty = repeat 0
-
-count :: [Seed] -> Table -> IO Int
-count seeds tab = do
-  counts <- sequence $ zipWith f tab seeds
+count :: Gen g a => [g] -> Table -> IO Int
+count gens tab = do
+  counts <- sequence $ zipWith f tab gens
   return $ minimum counts
   where
-    f row seed = do
-      (_, max) <- A.getBounds row
-      let i = seed `mod` (max + 1)
+    f row gen = do
+      bounds <- A.getBounds row
+      let i = index gen bounds
       A.readArray row i
 
-increment :: [Seed] -> Table -> IO Int
-increment seeds tab = do
-  counts <- sequence $ zipWith f tab seeds
+increment :: Gen g a => [g] -> Table -> IO Int
+increment gens tab = do
+  counts <- sequence $ zipWith f tab gens
   return $ minimum counts
   where
-    f row seed = do
-      (_, max) <- A.getBounds row
-      let i = seed `mod` (max + 1)
+    f row gen = do
+      bounds <- A.getBounds row
+      let i = index gen bounds
       count <- A.readArray row i
       A.writeArray row i (count + 1)
       return count
 
-roll :: Int -> [LCG] -> [Seed] -> [Seed]
-roll ch = zipWith $ rand ch
+roll :: Gen g a => a -> [g] -> [g]
+roll ch = map (next ch)
 
-minExtend = 20
+minExtend = 10
 
-step :: [LCG] -> Table -> [Seed] -> Int -> IO [Seed]
-step lcgs tab seeds ch = do
-  let next = roll ch lcgs seeds
-  n <- increment next tab
-  return $! if n > minExtend then next else empty
+step :: Gen g a => Table -> [g] -> a -> IO [g]
+step tab gens ch = do
+  let gens' = roll ch gens
+  n <- increment gens' tab
+  return $! if n > minExtend then gens' else reset gens'
 
 maxSize = 50
-minCount = 150
+minCount = 20
 choices = [1 .. 127]
---choices = map ord $ ['A' .. 'Z'] ++ ['a' .. 'z'] ++ ['0' .. '9'] ++ "{},-./= "
 
-search :: [LCG] -> Table -> IO ()
-search lcgs tab = go [] empty
+search :: Table -> [LCG] -> IO ()
+search tab = go []
   where
     go prefix _ | length prefix > maxSize = return ()
-    go prefix seeds = do
-      ns <- mapM (\ch -> let next = roll ch lcgs seeds
-                         in do n <- count next tab
-                               return (ch : prefix, next, n)) choices
+    go prefix gens = do
+      ns <- mapM (\ch -> let gens' = roll ch gens
+                         in do n <- count gens' tab
+                               return (ch : prefix, gens', n)) choices
       let ns' = filter ((>= minCount) . thd) ns
-      mapM_ (\(prefix', next, n) -> do
-                putStrLn $ show n ++ " " ++ (reverse $ map chr prefix')
-                go prefix' next) ns'
+      mapM_ (\(prefix', gens', n) -> do
+                putStrLn $ show n ++ " " ++ (reverse $ map (chr . fromIntegral) prefix')
+                go prefix' gens') ns'
 
 thd :: (a, b, c) -> c
 thd (_, _, x) = x                    
 
 bits = 10000
 height = length lcgs
-lcgs = [ LCG (2 ^ 32) 1664525 1013904223
-       , LCG (2 ^ 32) 22695477 1
-       , LCG (2 ^ 31) 1103515245 12345
+lcgs = [ LCG (2 ^ 32) 1664525 1013904223 1013904223
+       , LCG (2 ^ 32) 22695477 1 1
+       , LCG (2 ^ 31) 1103515245 12345 12345
        ]
 
 main :: IO ()
 main = do
   tab <- replicateM height $ A.newArray (0, bits - 1) 0 :: IO Table
   contents <- B.getContents
-  foldM_ (step lcgs tab) empty (map fromIntegral $ B.unpack contents)
-  search lcgs tab
-  --mapM_ (A.getElems >=> print) rows
+  foldM_ (step tab) lcgs (B.unpack contents)
+  search tab lcgs
+  --mapM_ (A.getElems >=> print) tab
