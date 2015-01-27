5d944098a353844ed0df9aca0f7668236a548950
Itai Zukerman <zukerman@math-hat.com>
2015-01-26 22:27:04 -0800

    * Use optparse-applicative. * Removed Setup stuff from .gitignore.

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 
+import Control.Applicative ((<$>), (<*>))
 import Control.DeepSeq (NFData, ($!!))
 import Control.Monad ((>=>), foldM, replicateM, zipWithM)
 import Data.Array.IO (IOUArray)
@@ -11,7 +12,15 @@ import Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString.Lazy as B
 import Data.Char (chr, ord)
 import Data.Word (Word64, Word8)
+import Options.Applicative
 import Text.Printf (printf)
+
+data SeqFreq = SeqFreq
+               { bits :: Int
+               , minExtend :: Int
+               , maxLength :: Int
+               , minCount :: Int
+               }
 
 class Gen g a | g -> a where
   reset :: g -> g
@@ -68,30 +77,24 @@ roll :: Gen g a => a -> [g] -> [g]
 roll ch = map (next ch)
 {-# INLINE roll #-}
 
-minExtend = 2
-
-step :: (NFData g, Gen g a) => Table -> [g] -> a -> IO [g]
-step tab gens ch = do
+step :: (NFData g, Gen g a) => SeqFreq -> Table -> [g] -> a -> IO [g]
+step args tab gens ch = do
   let gens' = roll ch gens
   n <- increment gens' tab
-  let next = if n >= minExtend
+  let next = if n >= minExtend args
              then gens'
              else resetAll gens'
   return $!! next
 
-maxSize = 50
-minCount = 100
-choices = [1 .. 127]
-
-search :: Table -> [LCG] -> IO ()
-search tab = go []
+search :: SeqFreq -> [Word8] -> Table -> [LCG] -> IO ()
+search args choices tab = go []
   where
-    go prefix _ | length prefix > maxSize = return ()
+    go prefix _ | length prefix > maxLength args = return ()
     go prefix gens = do
       ns <- mapM (\ch -> let gens' = roll ch gens
                          in do n <- count gens' tab
                                return (ch : prefix, gens', n)) choices
-      let ns' = filter ((>= minCount) . thd) ns
+      let ns' = filter ((>= minCount args) . thd) ns
       mapM_ (\(prefix', gens', n) -> do
                 putStrLn $ show n ++ " " ++ reverse (map (chr . fromIntegral) prefix')
                 go prefix' gens') ns'
@@ -99,7 +102,6 @@ search tab = go []
 thd :: (a, b, c) -> c
 thd (_, _, x) = x                    
 
-bits = 1000000
 height = length lcgs
 lcgs = [ LCG (2 ^ 32) 1664525 1013904223 0
        , LCG (2 ^ 32) 22695477 1 0
@@ -109,11 +111,19 @@ lcgs = [ LCG (2 ^ 32) 1664525 1013904223 0
        , LCG (2 ^ 32) 1140671485 12820163 0
        ]
 
+seqFreq :: Parser SeqFreq
+seqFreq = SeqFreq
+  <$> option auto (long "bits"      <> help "Number of bits in the sequence hash.")
+  <*> option auto (long "minextend" <> help "Minimum frequency before extending a sequence.")
+  <*> option auto (long "maxlength" <> help "Maximum sequence length.")
+  <*> option auto (long "mincount"  <> help "Minimum frequency for showing a sequence.")
+
 main :: IO ()
 main = do
-  tab <- replicateM height $ A.newArray (0, bits - 1) 0 :: IO Table
+  args <- execParser $ info (helper <*> seqFreq) fullDesc
+  tab <- replicateM height $ A.newArray (0, bits args - 1) 0 :: IO Table
   let root = resetAll lcgs
-  B.getContents >>= foldM (step tab) root . B.unpack
+  B.getContents >>= foldM (step args tab) root . B.unpack
   --mapM_ (A.getElems >=> print) tab
-  search tab root
+  search args [1 .. 127] tab root
 
